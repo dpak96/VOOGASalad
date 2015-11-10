90e2cdd0503ec71039705968c1665bc42b1c9acf
Jasper Hancock <jasper.hancock@duke.edu>
2015-11-09 22:33:21 -0500

    Feeble Authoring Environment Interface

---
+++ src/voogasalad_SquirtleSquad/IAuthoringController.java
@@ -0,0 +1,35 @@
+package voogasalad_SquirtleSquad;
+
+import javafx.scene.Node;
+
+
+public interface IAuthoringController {
+
+    /*
+     * Given a selected object, change its property
+     */
+    public void setProperty (Node obj);
+
+    /*
+     * Place an object from the palette onto the screen and reflect it in
+     * the model
+     */
+    public void placeObject (/* Something */);
+
+    /*
+     * Assign an action to a control
+     */
+    public void setControl (/* Map<KeyPress, Action> */);
+    
+    /*
+     * Sets the introductory splash screen
+     */
+    public void setSplash();
+    
+    /*
+     * Loads another level
+     */
+    public void loadLevel();
+    
+    
+}
