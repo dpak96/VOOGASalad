2f1bfdd7d7a4b42a74e5b31ec009acc3900e8003
Matthew Battles <matthewjbattles@gmail.com>
2015-11-11 14:48:40 -0500

    Create IncrementScore.java
    Written with GitHub, has not yet been properly formatted; may not compile.

---
+++ src/usecases/IncrementScore.java
@@ -0,0 +1,17 @@
+package usecases;
+
+import voogaSalad_SquirtleSquad.Game;
+
+public class IncrementScore {
+  
+  private double score;
+  
+  public IncrementScore(Game currentGame){
+    this.score = currentGame.score();
+  }
+  
+  public void addScore(double inc){
+    score += inc;
+  }
+
+}
