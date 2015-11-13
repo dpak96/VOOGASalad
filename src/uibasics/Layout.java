eeefa1452f001e63a7ccaaa703386608009d537f
Matt Battles <matthewjbattles@gmail.com>
2015-11-12 21:54:47 -0500

    Added MenuItem classes.

--- src/uibasics/Layout.java
+++ src/uibasics/Layout.java
@@ -3,5 +3,10 @@ package uibasics;
 import javafx.scene.layout.BorderPane;
 
 public class Layout extends BorderPane{
+	
+	public Layout(){
+		this.setCenter(new UIBasics());
+		this.setTop(new TopMenuBar());
+	}
 
 }
---
+++ src/uibasics/NewMenuItem.java
@@ -0,0 +1,25 @@
+//This entire file is a part of my masterpiece
+//Alex Rice
+package uibasics;
+
+import java.util.ResourceBundle;
+
+import javafx.scene.input.KeyCombination;
+import javafx.stage.Stage;
+
+public class NewMenuItem extends AbstractMenuItem {
+	
+	public NewMenuItem(ResourceBundle resource, Stage stage){
+		super(resource, stage);
+		this.setText(myResource.getString("NEW"));
+	    this.setAccelerator(KeyCombination.keyCombination("Ctrl+N"));
+
+	}
+	@Override
+	public void handle() {
+//		SlogoScene currScene = myController.getMyScene();
+//	    GUIManager currGuiManager = myController.getGuiManager();
+//	    new ModelController(myStage, currGuiManager, currScene);
+	}
+
+}
