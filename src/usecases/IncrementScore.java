c8b8d487a654d0c8e12401b58530644068b2569c
Matt Battles <matthewjbattles@gmail.com>
2015-11-12 19:14:58 -0500

    Fixed Use Cases.

--- src/usecases/IncrementScore.java
+++ src/usecases/IncrementScore.java
@@ -1,14 +1,14 @@
 package usecases;
 
-import voogaSalad_SquirtleSquad.Game;
+//import voogaSalad_SquirtleSquad.Game;
 
 public class IncrementScore {
   
   private double score;
   
-  public IncrementScore(Game currentGame){
-    this.score = currentGame.score();
-  }
+//  public IncrementScore(Game currentGame){
+//    this.score = currentGame.score();
+//  }
   
   public void addScore(double inc){
     score += inc;
