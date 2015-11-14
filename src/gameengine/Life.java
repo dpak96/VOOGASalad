c0196544571ef765f8c386789ce2b261bd1917c4
Daniel Pak <daniel1996@gmail.com>
2015-11-14 16:58:26 -0500

    Added life composition

---
+++ src/gameengine/Life.java
@@ -0,0 +1,39 @@
+package gameengine;
+
+public class Life {
+	private double health;
+	private int lives;
+	
+	public double getHealth(){
+		return health;
+	}
+	
+	public void setHealth(double value){
+		health = value;
+	}
+	
+	public int getLife(){
+		return lives;
+	}
+	
+	public void setLife(int value){
+		lives = value;
+	}
+	
+	public void removeHealth(double damage){
+		health -= damage;
+	}
+	
+	public void addHealth(double heal){
+		health += heal;
+	}
+	
+	public void removeLife(){
+		lives--;
+	}
+	
+	public void addLife(){
+		lives++;
+	}
+
+}
