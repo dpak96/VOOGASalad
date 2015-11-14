7a055dc449836547b00e7be098b71b6238e525d2
Daniel Pak <daniel1996@gmail.com>
2015-11-14 15:32:06 -0500

    Basic implementation of Article/Rules
    Filled out more information for Article/Rules

---
+++ src/gameengine/Rule.java
@@ -0,0 +1,14 @@
+package gameengine;
+
+import java.util.List;
+
+/**
+ * Hierarchy of classes that will define rules for the game
+ * @author danielpak
+ *
+ */
+public class Rule {
+	public void apply(List<Article> articles){
+		//Applies the rule on a given article
+	}
+}
