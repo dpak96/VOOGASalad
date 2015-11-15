7d9167693becf9a0e737fe235e60fae93f2626df
Rob Martorano (netid: rfm21) <rfm21@duke.edu>
2015-11-15 02:47:43 -0500

    Drag and Drop example

--- src/authoring/controller/AuthoringControllerInterface.java
+++ src/authoring/controller/AuthoringControllerInterface.java
@@ -1,7 +1,36 @@
 package authoring.controller;
 
+import javafx.scene.Node;
+import javafx.scene.image.Image;
+
 /**
  * Created by Rob on 11/15/15.
  */
 public interface AuthoringControllerInterface {
+
+    public void setToolBar(String toolBar);
+
+    public void selectFromToolBar();
+
+    public void expandToolBar();
+
+    public void createObj(String name);
+
+    public void createObjAndPlace(double x,double y, String name);
+
+    public void moveObj(double x, double y);
+
+    public void deleteObj(Node n);
+
+    public void editObj(Node n);
+
+    public void setSplash(Image img);
+
+
+
+
+    public void loadLevel();
+
+
+
 }
--- src/authoring/ui/drapanddrop/basicExample.java
+++ src/authoring/ui/drapanddrop/basicExample.java
@@ -1,7 +1,121 @@
 package authoring.ui.drapanddrop;
 
+import javafx.event.EventHandler;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import javafx.scene.input.*;
+
 /**
  * Created by Rob on 11/15/15.
  */
 public class basicExample {
+    final Text source = new Text(50, 100, "DRAG ME");
+    final Text target = new Text(250, 100, "DROP HERE");
+
+
+    public basicExample(){
+        source.setScaleX(2.0);
+        source.setScaleY(2.0);
+        target.setScaleX(2.0);
+        target.setScaleY(2.0);
+    }
+
+
+    public void drapNdrop(){
+        source.setOnDragDetected(new EventHandler <MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                /* drag was detected, start drag-and-drop gesture*/
+                System.out.println("onDragDetected");
+
+                /* allow MOVE transfer mode */
+                Dragboard db = source.startDragAndDrop(TransferMode.MOVE);
+
+                /* put a string on dragboard */
+                ClipboardContent content = new ClipboardContent();
+                content.putString(source.getText());
+                db.setContent(content);
+
+                event.consume();
+            }
+        });
+
+        target.setOnDragOver(new EventHandler <DragEvent>() {
+            @Override
+            public void handle(DragEvent event) {
+                /* data is dragged over the target */
+                System.out.println("onDragOver");
+
+                /* accept it only if it is  not dragged from the same node
+                 * and if it has a string data */
+                if (event.getGestureSource() != target &&
+                        event.getDragboard().hasString()) {
+                    /* allow for moving */
+                    event.acceptTransferModes(TransferMode.MOVE);
+                }
+
+                event.consume();
+            }
+        });
+
+        target.setOnDragEntered(new EventHandler <DragEvent>() {
+            @Override
+            public void handle(DragEvent event) {
+                /* the drag-and-drop gesture entered the target */
+                System.out.println("onDragEntered");
+                /* show to the user that it is an actual gesture target */
+                if (event.getGestureSource() != target &&
+                        event.getDragboard().hasString()) {
+                    target.setFill(Color.GREEN);
+                }
+                event.consume();
+            }
+        });
+
+        target.setOnDragExited(new EventHandler <DragEvent>() {
+            @Override
+            public void handle(DragEvent event) {
+                /* mouse moved away, remove the graphical cues */
+                target.setFill(Color.BLACK);
+                event.consume();
+            }
+        });
+
+
+        target.setOnDragDropped(new EventHandler <DragEvent>() {
+            @Override
+            public void handle(DragEvent event) {
+                /* data dropped */
+                System.out.println("onDragDropped");
+                /* if there is a string data on dragboard, read it and use it */
+                Dragboard db = event.getDragboard();
+                boolean success = false;
+                if (db.hasString()) {
+                    target.setText(db.getString());
+                    success = true;
+                }
+                /* let the source know whether the string was successfully
+                 * transferred and used */
+                event.setDropCompleted(success);
+
+                event.consume();
+            }
+        });
+
+
+        source.setOnDragDone(new EventHandler <DragEvent>() {
+            @Override
+            public void handle(DragEvent event) {
+                /* the drag-and-drop gesture ended */
+                System.out.println("onDragDone");
+                /* if the data was successfully moved, clear it */
+                if (event.getTransferMode() == TransferMode.MOVE) {
+                    source.setText("");
+                }
+                event.consume();
+            }
+        });
+    }
+
+
 }
