1d3fb4695304394a946d0841aeb4f4f7bf3c1d6f
Brandon Ho <brandonho36@gmail.com>
2015-11-17 00:54:41 -0500

    created new event class
    should encapsulate rules completely

---
+++ src/model/Event.java
@@ -0,0 +1,25 @@
+package model;
+import java.util.*;
+
+public class Event {
+
+	List<Article> myActors;
+	
+	List<Condition> myConditions;
+	List<Executable> myExecutables;
+	
+	public Event(List<Condition> conditions, List<Executable> executables){
+		myConditions = conditions;
+		myExecutables = executables;
+	}
+	
+	public void fire(){
+		for(Condition c : myConditions){
+			if(!c.check()) return;
+		}
+		for(Executable e : myExecutables){
+			e.execute();
+		}
+	}
+	
+}
