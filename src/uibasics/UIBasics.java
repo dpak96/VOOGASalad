8e2089694837acef7a468d74e74c394a3fd2fe89
steven katsohirakis <steven.katsohirakis@duke.edu>
2015-11-16 22:44:20 -0500

    updated UIBasics and added UIStackPane

--- src/uibasics/UIBasics.java
+++ src/uibasics/UIBasics.java
@@ -1,23 +1,40 @@
 package uibasics;
 
-import java.util.Observable;
+import model.Article;
+import state.turtle.TurtleState;
 
-import javafx.scene.input.KeyCode;
-import javafx.scene.layout.StackPane;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Observable;
+import java.util.Observer;
+
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
 import javafx.scene.shape.Rectangle;
 
-public class UIBasics extends Observable{
-	private StackPane myPane;
+public class UIBasics implements Observer {
+	private Pane myPane;
+	private HashMap<String, Article> myBackArticles;
+	private List<ImageView> myFrontArticles;
+	
 	public UIBasics(){
-		myPane = new StackPane();
+		myPane = new Pane();
 		myPane.getChildren().add(new Rectangle(50, 50, 50, 50));
-
+		myBackArticles = new HashMap<String, Article>();
+		myFrontArticles = new ArrayList<ImageView>();
 	}
 	
-	public StackPane getPane(){
+	public Pane getPane(){
 		return myPane;
 	}
-	
 
+	@SuppressWarnings("unchecked")
+	@Override
+	public void update(Observable o, Object arg) {
+		myBackArticles = ((HashMap<String, Article>) arg);
+		myBackArticles.
+	}	
+	
 	
 }
