53c856e5c7c1aea64771ab6f823cb2f281aa23e5
Daniel Pak <daniel1996@gmail.com>
2015-11-18 01:12:17 -0500

    Added event creation

--- src/model/Event.java
+++ src/model/Event.java
@@ -3,15 +3,31 @@ import java.util.*;
 
 public class Event {
 
-	String myName;
+	private String myName;
 	
-	List<Condition> myConditions;
-	List<Executable> myExecutables;
+	private List<Condition> myConditions;
+	private List<Executable> myExecutables;
 	
 	public Event(String name, List<Condition> conditions, List<Executable> executables){
 		myName = name;
 		myConditions = conditions;
 		myExecutables = executables;
+	}
+	
+	public void addCondition(Condition cond){
+		myConditions.add(cond);
+	}
+	
+	public void addAllConditions(List<Condition> conds){
+		myConditions.addAll(conds);
+	}
+	
+	public void addExecutaale(Executable exec){
+		myExecutables.add(exec);
+	}
+	
+	public void addAllExecutables(List<Executable> execs){
+		myExecutables.addAll(execs);
 	}
 	
 	public void fire(){
