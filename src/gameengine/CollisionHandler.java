c370854d0392b62c0dd049692714fcfe1569ea30
Wuming Zhang <zhangwuming950624@gmail.com>
2015-11-18 04:48:35 -0500

    Created CollisionHandler class
    done

---
+++ src/gameengine/CollisionHandler.java
@@ -0,0 +1,18 @@
+package gameengine;
+
+import model.Article;
+
+public class CollisionHandler {
+	Article myArticle;
+	Article myCollided;
+	public CollisionHandler(Article article, Article collided) {
+		// TODO Auto-generated constructor stub
+		myArticle = article;
+		myCollided = collided;
+	}
+	
+	public void collide(){
+		
+	}
+	
+}
