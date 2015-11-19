98954e4493a99abcad79d89ddc09f69a6f313657
Kevin Wang <kw197@duke.edu>
2015-11-18 22:36:21 -0500

    add properties to buttons

--- src/authoring/ui/toolbar/PlatformButton.java
+++ src/authoring/ui/toolbar/PlatformButton.java
@@ -5,21 +5,22 @@ import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
-public class PlatformButton extends ToolbarButton{
 
-    public PlatformButton () {
-        super();
-        this.setGraphic(super.setImage(toolbarProperties.getString("PLATFORM")));
-    }
+public class PlatformButton extends ToolbarButton {
+  private final String NAME = "PLATFORM";
 
-    @Override
-    public void placeYourObject () {
-            
-            EnemyProperties propTest=new EnemyProperties();
-            propTest.showMenu("Enemy Property Editor");
-    }
+  public PlatformButton() {
+    super();
+    this.setGraphic(super.setImage(toolbarProperties.getString(NAME)));
+    this.setName(NAME);
+    this.setImageName(super.toolbarProperties.getString(NAME));
+  }
 
-   
-    
-    
+  @Override
+  public void placeYourObject() {
+
+    EnemyProperties propTest = new EnemyProperties();
+    propTest.showMenu("Enemy Property Editor");
+  }
+
 }
