b7c3af289b05f636126399edb51fc16109467708
Rob Martorano (netid: rfm21) <rfm21@duke.edu>
2015-11-19 07:29:39 -0500

    SAVING WITH CALVIN

--- src/model/XMLutility/xmlUtility.java
+++ src/model/XMLutility/xmlUtility.java
@@ -1,10 +1,15 @@
 package model.XMLutility;
+import java.io.File;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Observable;
 
 import com.thoughtworks.xstream.XStream;
 import com.thoughtworks.xstream.io.xml.DomDriver;
 
+import javafx.stage.FileChooser;
+import javafx.stage.Window;
 import model.Article;
 import model.Model;
 
@@ -16,34 +21,45 @@ public class xmlUtility {
 		myModel = model;
 	}
 	
-	public void saveModel(String gameName) {
+	public void saveModel(Window window) {
+		FileChooser myFileChooser = new FileChooser();
+		FileChooser.ExtensionFilter extensionFilter =
+				new FileChooser.ExtensionFilter("Java files (*.xml)", "*.xml");
+		myFileChooser.getExtensionFilters().add(extensionFilter);
+		myFileChooser.setTitle("Howdy");
+		File game = myFileChooser.showSaveDialog(window);
+		try {
+			save(game);
+//	      o.writeObject(myController.getMyScene().getAllData().get(0));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+		public void save(File game){
 		FileOutputStream fos = null;
+
+		myStream.omitField(Observable.class, "obs");
+		myStream.omitField(Observable.class, "changed");
 		for(Article a: myModel.getArticles()) {
 			a.destroyBitMap();
 			System.out.println("destroyed");
 		}
 		myModel.getViewpoint().destroyBitMap();
 		myModel.getCharacter().destroyBitMap();
-	    try{            
-	        String xml = myStream.toXML(myModel);
-	        
-	        fos = new FileOutputStream(gameName + ".xml");
-	        fos.write("<?xml version=\"1.0\"?>".getBytes("UTF-8"));
-	        byte[] bytes = xml.getBytes("UTF-8");
-	        fos.write(bytes);
-
-	    }catch (Exception e){
-	        System.err.println("Error in XML Write: " + e.getMessage());
-	    }
-	    finally{
-	        if(fos != null){
-	            try{
-	                fos.close();
-	            }catch (IOException e) {
-	                e.printStackTrace();
-	            }
-	        }
-	    }
+		try{
+			myStream.toXML(myModel, new FileWriter(game));
+		}catch (Exception e){
+			System.err.println("Error in XML Write: " + e.getMessage());
+		}
+		finally{
+			if(fos != null){
+				try{
+					fos.close();
+				}catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
 	}
 	
 	public static void main (String args[]) {
@@ -52,7 +68,7 @@ public class xmlUtility {
 		Article test = new Article("Goomba.png", 100, 100, true);
 		model.addArticle(test);
 		xmlUtility xml = new xmlUtility(model);
-		xml.saveModel("test");
+		//xml.saveModel("test");
 		System.out.println("saved");
 	}
 
