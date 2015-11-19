a4967e1615bb2a315dd9e352cb648b46cf384293
Calvin Chueh <chchueh24@gmail.com>
2015-11-19 01:25:38 -0500

    added xmlsaver for XML

--- src/model/Model.java
+++ src/model/Model.java
@@ -33,6 +33,13 @@ public class Model extends Observable{
 		return current;
 	}
 	
+	public void initialize() {
+		myEvents = new ArrayList<Event>();
+		myButtonMap = new HashMap<String, List<Event>>();
+		myArticles = new ArrayList<Article>();
+		myViewpoint = new Article("GoombaRefinedAGain.png", 100, 100);
+		myCharacter = new Article("GoombaRefinedAGain.png", 100, 100);
+	}
 	private boolean articleContainsPoint(Article article, double x, double y) {
 		return x > article.getX() && x < article.getX()+article.getWidth()
 		&& y > article.getY() && y < article.getY()+article.getHeight();
