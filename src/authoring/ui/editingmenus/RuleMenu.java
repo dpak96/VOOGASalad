8a10212f6152e555cc0578d67fa90fb20a999301
Jasper Hancock <jasper.hancock@duke.edu>
2015-11-19 02:27:25 -0500

    dynamic parameters in rule menu

--- src/authoring/ui/editingmenus/RuleMenu.java
+++ src/authoring/ui/editingmenus/RuleMenu.java
@@ -1,6 +1,9 @@
 package authoring.ui.editingmenus;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import com.sun.naming.internal.ResourceManager;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
@@ -31,14 +34,23 @@ public class RuleMenu extends AuthoringMenu{
         ComboBox ruleTypeBox=new ComboBox();
         menuPane.add(ruleTypeBox, 2, 1);
         ruleTypeBox.getItems().add("Gravity");
-        ruleTypeBox.setOnAction(e -> addParameterFields(menuPane));
+        ruleTypeBox.setOnAction(e -> addParameterFields(ruleTypeBox,menuPane));
         
     }
     
-    public void addParameterFields(GridPane menuPane){
-      
-        menuPane.add(new Label("Gravity Value"), 1, 2);
-        menuPane.add(new TextField(), 2, 2);
+    public void addParameterFields(ComboBox ruleBox,GridPane paramGrid){
+      ModelController model=new ModelController(null);
+      Map<String, Class<?>> ruleParams=model.getParameters("model."+resourcemanager.ResourceManager.getResourceManager().getPm().getResourceMap().get("rules").getString(ruleBox.getValue().toString()));
+      int rowIndex=2;
+      for(String key: ruleParams.keySet())
+      {
+          super.componentAdder.makeLabel(paramGrid, 1, rowIndex, key);
+          if(ruleParams.get(key).getName()=="model.Article")
+              super.componentAdder.makeComboBox(paramGrid, 2, rowIndex++);
+          super.componentAdder.makeField(paramGrid, 2, rowIndex++);
+          System.out.println(ruleParams.get(key).getName());
+      }
+     
         
     }
    
