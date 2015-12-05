69aa6b8a6814fa3ae7ad3979688aaf051521473f
Rob Martorano (netid: rfm21) <rfm21@duke.edu>
2015-12-05 03:58:55 -0500

    Incredible paired programming by Rob and Kevin for editing article properties

--- src/authoring/backend/PropertyEditor.groovy
+++ src/authoring/backend/PropertyEditor.groovy
@@ -1,6 +1,17 @@
 package authoring.backend
 
+import model.article.Article
+
+import java.lang.reflect.Method
+
 
 class PropertyEditor {
+
+
+    public editProperty(String function,x, Article article) {
+        article."$function"(x);
+    }
+
+
 
 }
