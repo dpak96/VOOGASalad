fff52f7ec11b06a0177816bc98c42ce80b5b2cad
steven katsohirakis <steven.katsohirakis@duke.edu>
2015-12-05 20:52:58 -0500

    fix for position for long elements

--- src/front/commands/Position.java
+++ src/front/commands/Position.java
@@ -1,15 +1,21 @@
 package front.commands;
 
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import model.article.Article;
 import model.controller.ModelController;
+import resourcemanager.ResourceManager;
 
 public class Position extends AbstractCommand {
 
 	@Override
 	public void update(Article article, ModelController modelController, ImageView front) {
-		front.setX(article.getX()-modelController.getViewpoint().getX());
-		front.setY(article.getY()-modelController.getViewpoint().getY());
+		Image img = (Image) ResourceManager.getResourceManager().getResource("ImageManager", article.getImageFile());
+		int iSizeX = (int) (article.getWidth()/img.getWidth());
+		for (int i = 0; i < iSizeX; i++) {
+			front.setX(article.getX()-modelController.getViewpoint().getX()+(i*img.getWidth()));
+			front.setY(article.getY()-modelController.getViewpoint().getY());
+		}
 	}
 
 }
