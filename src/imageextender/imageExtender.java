4ff338e449801ea531b87e8462414061f3aa547c
Kevin Wang <kw197@duke.edu>
2015-12-06 01:54:56 -0500

    initial finished implementation for extender

--- src/imageextender/imageExtender.java
+++ src/imageextender/imageExtender.java
@@ -1,17 +1,25 @@
 package imageextender;
 
+import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 
+import javafx.embed.swing.SwingFXUtils;
 import javafx.scene.image.Image;
 
 public class imageExtender {
 	public Image extendImage(int times, Image im){
-		Image ans = null;
-		int w = (int) im.getWidth();
-		int h = (int) im.getHeight();
-		BufferedImage combined = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
-
+		java.awt.image.BufferedImage awtIm = SwingFXUtils.fromFXImage(im, null);
+		int w = awtIm.getWidth();
+		int h = awtIm.getHeight();
 		
+		BufferedImage combined = new BufferedImage(times*w, h, BufferedImage.TYPE_INT_ARGB);
+		Graphics g = combined.getGraphics();
+		
+		for(int i = 0; i < times; i++){
+			g.drawImage(awtIm, i*w, 0, null);
+		}
+
+		javafx.scene.image.Image ans = SwingFXUtils.toFXImage(combined, null);		
 		return ans;
 	}
 }
