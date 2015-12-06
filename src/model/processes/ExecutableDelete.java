84f68001a4a9a2ff625c44ab0ed2323741dab2f5
Brandon Ho <brandonho36@gmail.com>
2015-12-06 00:03:55 -0500

    created delete executable

---
+++ src/model/processes/ExecutableDelete.java
@@ -0,0 +1,24 @@
+package model.processes;
+
+import java.util.Map;
+
+import model.Model;
+
+public class ExecutableDelete extends Executable{
+	
+	private Model myModel;
+
+	public ExecutableDelete(Map<String, Object> parameters) {
+		super(parameters);
+	}
+	
+	public void setModel(Model m){
+		myModel = m;
+	}
+
+	@Override
+	public void execute() {
+		myModel.removeArticle(myActor);
+	}
+
+}
