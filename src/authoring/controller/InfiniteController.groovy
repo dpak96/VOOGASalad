f79c0104421593a9d53b0fe6b85d5b72cd45a446
Rob Martorano (netid: rfm21) <rfm21@duke.edu>
2015-12-07 04:35:21 -0500

    Beginnig infinite

--- src/authoring/controller/InfiniteController.groovy
+++ src/authoring/controller/InfiniteController.groovy
@@ -1,7 +1,19 @@
 package authoring.controller
 
-/**
- * Created by Rob on 12/7/15.
- */
+
 class InfiniteController {
+
+    public InfiniteController(){
+
+    }
+
+
+    public addInfinite(){
+
+    }
+
+    public genInfinite(){
+
+    }
+
 }
