209fd2aa58878dba23a6544b7d473ff02d2622b4
Alex Rice <akr24@duke.edu>
2015-12-07 19:34:49 -0500

    score saving functionality almost complete

--- src/game/player/HighScoresDialog.java
+++ src/game/player/HighScoresDialog.java
@@ -1,7 +1,36 @@
 package game.player;
 
+import java.io.IOException;
+import java.util.Map;
+
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.xml.sax.SAXException;
+
 import javafx.scene.control.Dialog;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.GridPane;
 
 public class HighScoresDialog extends Dialog {
-
+	
+	private GridPane myScores;
+	private String myGameName;
+	
+	public HighScoresDialog(){
+		super();
+		myScores = new GridPane();
+		this.getDialogPane().setContent(myScores);
+	}
+	
+	public void init(String name) throws SAXException, IOException, ParserConfigurationException{
+		myGameName = name;
+		HighScoresXMLReader read = new HighScoresXMLReader(myGameName);
+		Map<Integer,String> scores = read.getScoreMap();
+		//myScores.add
+        //score.set
+		
+		
+		
+	}
 }
