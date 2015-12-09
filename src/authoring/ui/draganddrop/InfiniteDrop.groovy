4283621b50219bd3390058f0f9d0099014a8b4c9
Rob Martorano (netid: rfm21) <rfm21@duke.edu>
2015-12-09 02:51:55 -0500

    Drag and Drop

---
+++ src/authoring/ui/draganddrop/InfiniteDrop.groovy
@@ -0,0 +1,22 @@
+package authoring.ui.draganddrop
+
+import authoring.controller.AuthoringController
+
+/**
+ * Created by Rob on 12/9/15.
+ */
+public class InfiniteDrop extends AbstractDragAndDrop {
+
+    public InfiniteDrop(AuthoringController ac){
+        super(ac);
+    }
+
+    @Override
+    protected void setListeners(AuthoringController authoringController) {
+        this.setOnDragDropped({});
+        this.setOnDragOver({});
+        this.setOnMouseClicked({});
+    }
+
+
+}
