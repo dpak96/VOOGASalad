d55c5ae1d778a7a06b82a421046274450b02c733
Kevin Wang <kw197@duke.edu>
2015-12-09 03:18:45 -0500

    add flappy preset

---
+++ src/authoring/controller/presets/FlappyPreset.groovy
@@ -0,0 +1,10 @@
+package authoring.controller.presets
+
+import authoring.controller.AuthoringController
+import model.controller.ModelController
+
+class FlappyPreset extends Preset {
+	public FlappyPreset(AuthoringController ac, ModelController mc) {
+		super(ac, mc);
+	}
+}
