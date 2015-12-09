18a77e1be7f3a87366275954ce7f6def79a9554a
Rob Martorano (netid: rfm21) <rfm21@duke.edu>
2015-12-09 00:49:39 -0500

    Starting infinite

--- src/authoring/controller/InfiniteController.groovy
+++ src/authoring/controller/InfiniteController.groovy
@@ -1,12 +1,42 @@
 package authoring.controller
 
+import model.article.Article
+import model.controller.ModelController
+
 
 class InfiniteController {
 
+    private AuthoringController myAuthoringController;
+    private ModelController myModelController;
+    private Map<Article,Double> randomGenMap;
+    private List<List<Article>> constantGen;
+    private List<Article> tempList;
 
-    public InfiniteController(AuthoringController ac){
-
+    public InfiniteController(AuthoringController ac, ModelController mc){
+        myAuthoringController = ac;
+        myModelController = mc;
+        randomGenMap = new HashMap<>();
+        constantGen = new ArrayList<>();
+        tempList = new ArrayList<>();
     }
+
+    public addToRandom(Article n, double d){
+        randomGenMap.put(n, d);
+    }
+
+    public getRandomMap(){
+        return randomGenMap;
+    }
+
+    public addIndConstant(Article n){
+        tempList.add(n);
+    }
+
+    public addSetToConstant(){
+        constantGen.add(tempList);
+        tempList = new ArrayList<>();
+    }
+
 
 
     public addInfinite(){
@@ -15,6 +45,15 @@ class InfiniteController {
 
     public genInfinite(){
 
+    }
+
+    public genRandom(){
+        randomGenMap = new HashMap<>();
+    }
+
+    public genConstant(){
+
+        constantGen = new ArrayList<>();
     }
 
 }
